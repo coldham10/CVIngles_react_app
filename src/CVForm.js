8fb47be764c1d2b78d3e994fe221f3fe5da8bd76
Christopher Oldham <coldham10@gmail.com>
2020-02-10 14:45:42 -0500

    First ability to add arbitrary fields

--- src/CVForm.js
+++ src/CVForm.js
@@ -6,21 +6,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function CVEntry(props) {
-    console.log(props);
-    return (
-        <Form.Group>
-            <Form.Label>{props.displayName}</Form.Label>
-            <Form.Control type={props.type}>
-            </Form.Control>
-        </Form.Group>
-    );
+    if (props.section === "datos" && props.fixed) {
+        return (
+            <Form.Group>
+                <Form.Label>{props.displayName}</Form.Label>
+                <Form.Control type={props.type}>
+                </Form.Control>
+            </Form.Group>
+        );
+    }
+    if (props.section === "datos") {
+        return (
+            <InputGroup className="mb-3">
+                <DropdownButton
+                    as={InputGroup.Prepend}
+                    variant="outline-secondary"
+                    title="Dropdown"
+                    id="input-group-dropdown-1"
+                >
+                  <Dropdown.Item href="#">Action</Dropdown.Item>
+                  <Dropdown.Item href="#">Another action</Dropdown.Item>
+                  <Dropdown.Item href="#">Something else here</Dropdown.Item>
+                  <Dropdown.Divider />
+                  <Dropdown.Item href="#">Separated link</Dropdown.Item>
+                </DropdownButton>
+              <Form.Control type={props.type} />
+            </InputGroup>
+        );
+    }
 }
 
 function CVSection(props) {
-    const entries = props.data.entries.map((entry) =>
-        <CVEntry {...entry}/>
+    const entries = props.data.entries.map((entry, i) =>
+        <CVEntry key={i} section={props.data.name} {...entry}/>
     );
     return (
         <Container className="cv-form-section border rounded">
@@ -38,9 +61,8 @@ class CVForm extends React.Component {
         this.state = this.defaults.formDefaults;
     }
 
-    addEntry(section) {
+    addEntry(section, idx) {
         let newEntry;
-        console.log('clicked');
         switch(section.name) {
             case "datos":
                 newEntry = this.defaults.entryDefaults.datos;
@@ -54,36 +76,26 @@ class CVForm extends React.Component {
             default:
                 throw new Error("Cannot add entry: section missing");
         }
-        section.entries = section.entries.concat(newEntry);
+        let replacementSection = {...section};
+        replacementSection.entries = section.entries.concat(newEntry);
+        this.setState({sections: 
+            this.state.sections.slice(0,idx)
+            .concat(replacementSection)
+            .concat(this.state.sections.slice(idx+1))}
+        );
     }
 
     render() {
-        const sections = this.state.sections.map((section) => {
+        const sections = this.state.sections.map((section, idx) => {
             return (
-                <CVSection data={section} addEntry={() => this.addEntry(section)}/>
+                <CVSection key={idx} data={section} addEntry={() => this.addEntry(section, idx)}/>
             );
         });
         return (
-            <Container>
+            <Container >
             <Form className="cv-form border rounded">
             {/*TODO: Validation, separate checkout page*/}
             {sections}
-
-            {/*            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-                </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
-                  <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group> */}
                 <Button variant="primary" type="submit">
                   Submit
                 </Button>
