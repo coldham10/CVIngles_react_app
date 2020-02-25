49b20e88cd7648353de3e73ecc6eeb9ff4b310ea
Christopher Oldham <coldham10@gmail.com>
2020-02-25 16:07:22 -0500

    App state updated when navigating from Home to Choice and Choice to Form

--- src/Choice.js
+++ src/Choice.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { withRouter } from 'react-router-dom';
+
 import Container from 'react-bootstrap/Container';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
@@ -15,9 +17,12 @@ class Choice extends React.Component{
         this.state = {'options': props.options};
         this.sendOptions = props.setOptions;
     }
-    /*TODO: update state in such a way that disables excluded combos;
-    * highlight selected with bg="primary"
-    * Standardise height once all written out*/
+
+    submit() {
+        this.sendOptions(this.state.options);
+        this.props.history.push('/enviar');
+        window.scrollTo(0,0);
+    }
 
     render() {
         return (
@@ -36,12 +41,9 @@ class Choice extends React.Component{
                 <Card.Body>
                     <Card.Title><Row><Col>De Lujo</Col><Col xs={4}>$40</Col></Row></Card.Title>
                     <Card.Text>
-                        <p>
                         Perfeccionamos el contenido a mostrar sus habilidades, quitamos lo que no es necesario y consultamos con usted hasta que está perfecto.
-                        </p>
-                        <p>
+                        <br /><br />
                         <strong>Incluye dos revisiones, formato impesionante, edición de copia y traducción si es necesario.</strong>
-                        </p>
                     </Card.Text>
                 </Card.Body>
             </Card>
@@ -50,19 +52,16 @@ class Choice extends React.Component{
             <Col>
             <Card
                 className="mx-auto my-4"
-                style={{ width: '18rem' , height: '32rem', backgroundColor: (this.state.options.service === 'n' ? "#5ca4a9" : "white")}}
-                onClick={() => this.setState({'options': Object.defineProperty({...this.state.options}, 'service', {'value': 'n'})})}
+                style={{ width: '18rem' , height: '32rem', backgroundColor: (this.state.options.service === 't' ? "#5ca4a9" : "white")}}
+                onClick={() => this.setState({'options': Object.defineProperty({...this.state.options}, 'service', {'value': 't'})})}
             >
                 <Card.Img variant="top" src="./shrink-english.jpg" />
                 <Card.Body>
                     <Card.Title><Row><Col>Traducción Profesional</Col><Col xs={4}>$20</Col></Row></Card.Title>
                     <Card.Text>
-                        <p>
                         Su hoja de vida es traducido de español por nuestro equipo de hispanohablantes bilingües y luego editado por nuestro equipo angloparlante.
-                        </p>
-                        <p>
+                        <br /><br />
                         <strong>Incluye también formato impesionante y edición de copia.</strong>
-                        </p>
                     </Card.Text>
                 </Card.Body>
             </Card>
@@ -78,12 +77,9 @@ class Choice extends React.Component{
                 <Card.Body>
                     <Card.Title><Row><Col>Inglés Nativo</Col><Col xs={4}>$15</Col></Row></Card.Title>
                     <Card.Text>
-                        <p>
                         Nuestro equipo de editores nativos angloparlantes modifica su hoja de vida hasta que el inglés está fluido y nativo.
-                        </p>
-                        <p>
+                        <br /><br />
                         <strong>Incluye también formato impesionante.</strong>
-                        </p>
                     </Card.Text>
                 </Card.Body>
             </Card>
@@ -92,11 +88,11 @@ class Choice extends React.Component{
             </Container>
 
             <Container className="pb-5">
-            <Button variant="secondary" className="mx-auto">¡Cree su CV!</Button>
+            <Button variant="secondary" className="mx-auto" onClick={() => this.submit()}>¡Cree su CV!</Button>
             </Container>
             </Container>
         );
     }
 }
 
-export default Choice;
+export default withRouter(Choice);
