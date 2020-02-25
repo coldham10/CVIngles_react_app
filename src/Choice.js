827a6e36227735a15bc7bf1e9b1eb31a39acc406
Christopher Oldham <coldham10@gmail.com>
2020-02-25 13:56:26 -0500

    Updated service options

--- src/Choice.js
+++ src/Choice.js
@@ -29,15 +29,19 @@ class Choice extends React.Component{
             <Col>
             <Card
                 className={'mx-auto my-4'}
-                style={{ width: '18rem' , height: '25rem', backgroundColor: (this.state.options.service === 'p' ? "#5ca4a9" : "white")}}
+                style={{ width: '18rem' , height: '32rem', backgroundColor: (this.state.options.service === 'p' ? "#5ca4a9" : "white")}}
                 onClick={() => this.setState({'options': Object.defineProperty({...this.state.options}, 'service', {'value': 'p'})})}
             >
                 <Card.Img variant="top" src="./shrink-suit.jpg" />
                 <Card.Body>
-                    <Card.Title><Row><Col>Profesional</Col><Col xs={4}>$35</Col></Row></Card.Title>
+                    <Card.Title><Row><Col>De Lujo</Col><Col xs={4}>$40</Col></Row></Card.Title>
                     <Card.Text>
-                        Juntos... Lorem ipsum dolor amet organic vaporware next level iPhone cliche vinyl.
-                        Readymade flannel aliqua ennui normcore adaptogen.
+                        <p>
+                        Perfeccionamos el contenido a mostrar sus habilidades, quitamos lo que no es necesario y consultamos con usted hasta que está perfecto.
+                        </p>
+                        <p>
+                        <strong>Incluye dos revisiones, formato impesionante, edición de copia y traducción si es necesario.</strong>
+                        </p>
                     </Card.Text>
                 </Card.Body>
             </Card>
@@ -46,15 +50,19 @@ class Choice extends React.Component{
             <Col>
             <Card
                 className="mx-auto my-4"
-                style={{ width: '18rem' , height: '25rem', backgroundColor: (this.state.options.service === 'n' ? "#5ca4a9" : "white")}}
+                style={{ width: '18rem' , height: '32rem', backgroundColor: (this.state.options.service === 'n' ? "#5ca4a9" : "white")}}
                 onClick={() => this.setState({'options': Object.defineProperty({...this.state.options}, 'service', {'value': 'n'})})}
             >
                 <Card.Img variant="top" src="./shrink-english.jpg" />
                 <Card.Body>
-                    <Card.Title><Row><Col>Nativo</Col><Col xs={4}>$15</Col></Row></Card.Title>
+                    <Card.Title><Row><Col>Traducción Profesional</Col><Col xs={4}>$15</Col></Row></Card.Title>
                     <Card.Text>
-                        Lorem ipsum dolor amet organic vaporware next level iPhone cliche vinyl.
-                        Gluten-free occaecat microdosing post-ironic cred hammock succulents kombucha.
+                        <p>
+                        Su hoja de vida es traducido de español por nuestro equipo de hispanohablantes bilingües y luego editado por nuestro equipo angloparlante.
+                        </p>
+                        <p>
+                        <strong>Incluye también formato impesionante y edición de copia.</strong>
+                        </p>
                     </Card.Text>
                 </Card.Body>
             </Card>
@@ -63,73 +71,24 @@ class Choice extends React.Component{
             <Col>
             <Card
                 className="mx-auto my-4"
-                style={{ width: '18rem' , height: '25rem', backgroundColor: (this.state.options.service === 'i' ? "#5ca4a9" : "white")}}
+                style={{ width: '18rem' , height: '32rem', backgroundColor: (this.state.options.service === 'i' ? "#5ca4a9" : "white")}}
                 onClick={() => this.setState({'options': Object.defineProperty({...this.state.options}, 'service', {'value': 'i'})})}
             >
                 <Card.Img variant="top" src="./shrink-splash.jpg" />
                 <Card.Body>
-                    <Card.Title><Row><Col>Impresionante</Col><Col xs={4}>$8</Col></Row></Card.Title>
+                    <Card.Title><Row><Col>Inglés Nativo</Col><Col xs={4}>$10</Col></Row></Card.Title>
                     <Card.Text>
-                        Quis selfies edison bulb scenester godard lorem.
-                        Viral 8-bit four dollar toast, woke pariatur lomo raw denim labore. Bitters ugh 90's typewriter offal jianbing.
+                        <p>
+                        Nuestro equipo de editores nativos angloparlantes modifica su hoja de vida hasta que el inglés está fluido y nativo.
+                        </p>
+                        <p>
+                        <strong>Incluye también formato impesionante.</strong>
+                        </p>
                     </Card.Text>
                 </Card.Body>
             </Card>
             </Col>
             </Row>
-            </Container>
-
-            <Container className="border rounded my-5 choice-group">
-            <h3 className="pb-1 pt-3 px-2">Idiomas</h3>
-            <Row>
-            <Col>
-            <Card
-                className="mx-auto my-4"
-                style={{ width: '18rem' , height: '14rem', backgroundColor: ((this.state.options.inLang === 'en' && this.state.options.outLang === 'en') ? "#5ca4a9" : "white")}}
-                onClick={() => this.setState({'options': Object.defineProperties({...this.state.options}, {'inLang': {'value': 'en'}, 'outLang': {'value':'en'}})})}
-            >
-                <Card.Body>
-                    <Card.Title><Row><Col>Todo inglés</Col><Col xs={4}>$0</Col></Row></Card.Title>
-                    <Card.Text className="py-3">
-                        Escribe todo en inglés y el CV está en inglés.
-                        Usted tiene control mas directo con el sentido del producto final.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-            </Col>
-
-            <Col>
-            <Card
-                className="mx-auto my-4"
-                style={{ width: '18rem' , height: '14rem', backgroundColor: ((this.state.options.inLang === 'es' && this.state.options.outLang === 'en') ? "#5ca4a9" : "white")}}
-                onClick={() => this.setState({'options': Object.defineProperties({...this.state.options}, {'inLang': {'value': 'es'}, 'outLang': {'value':'en'}})})}
-            >
-                <Card.Body>
-                    <Card.Title><Row><Col>Traducido</Col><Col xs={4}>$8</Col></Row></Card.Title>
-                    <Card.Text>
-                        Usted puede escribir algunas o todas partes en español, y nosotros lo traduciremos a ingles nativo.{' '} 
-                        <strong>Con traducion es necesario incluir el servicio Nativo o Profesional</strong>.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-            </Col>
-
-            <Col>
-            <Card
-                className="mx-auto my-4"
-                style={{ width: '18rem' , height: '14rem', backgroundColor: ((this.state.options.inLang === 'es' && this.state.options.outLang === 'es') ? "#5ca4a9" : "white")}}
-                onClick={() => this.setState({'options': Object.defineProperties({...this.state.options}, {'inLang': {'value': 'es'}, 'outLang': {'value':'es'}})})}
-            >
-                <Card.Body>
-                    <Card.Title><Row><Col>Todo español</Col><Col xs={4}>$0</Col></Row></Card.Title>
-                    <Card.Text className="py-3">
-                        Escribe todo en español y el CV está en español. Si el CV final está en español, <strong>no podemos hacer el servicio Nativo ni Profesional</strong>.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-            </Col>
-            </Row>
-
             </Container>
 
             <Container className="pb-5">
--- src/Home.js
+++ src/Home.js
@@ -80,6 +80,7 @@ function Home() {
             </Carousel.Item>
         </Carousel>
         </Container>
+
         <Container className="my-3 p-3" id="servicios">
         <h1 className="my-3">Que hacemos?</h1>
         <p className="p-2">
@@ -88,7 +89,7 @@ function Home() {
         </p>
         <p className="p-2">
         Nuestro <strong>equipo de editores nativos angloparlantes</strong> puede modificar su hoja de vida para decir lo que quiere como si fuera inglés su idioma primera.
-        Si no esta comodo o tienes un sentido dificil traducir a inglés, también tenemos hispanohablantes bilingües que pueden trabajar mano a mano con nuestros editores para traducir y refinar exactamente que quiere decir.
+        Si no esta comodo o tiene un sentido dificil traducir a inglés, también tenemos hispanohablantes bilingües que pueden trabajar mano a mano con nuestros editores para traducir y refinar exactamente que quiere decir.
         </p>
         <p className="p-2">
         Además con nuestro servicio Profesional, asignamos un editor personal a usted para revisar la estructra de su CV, enfocarlo en sus habilidades mas importantes, y hablamos con usted hasta que juntos hemos hecho <strong>una hoja de vida que muestra lo mejor que tiene a su proximo empleador</strong>.
@@ -96,13 +97,13 @@ function Home() {
         </Container>
 
         <Container fluid className="m-0 main-c2">
-        <Container className="mt-3 p-3 ">
-        <h1 className="my-3">Nuestros opciones</h1>
+        <Container className="mt-3 py-3 px-1 p-md-3">
+        <h1 className="my-3 px-3">Nuestros opciones</h1>
 
         <Container className="py-3 px-1">
         <Card bg="light">
             <Card.Header>
-                <h4 className="p-1">Profesional</h4>
+                <h4 className="p-1">Servicio de Lujo{/*Profesional, Perfeccion de contenido?*/}</h4>
             </Card.Header>
             <Card.Body>
                 <p className="p-2 pt-md-3">
@@ -113,7 +114,7 @@ function Home() {
                 Epitomamos los detalles mas importantes a empleadores en su región elegida y conversamos con usted para muestrar al mundo todo lo que puede hacer.
                 </p>
                 <p className="p-2">
-                <strong>Incluye también formato Impesionante y el servicio Nativo.</strong>
+                <strong>Incluye también formato impesionante, edición de copia y traducción si es necesario.</strong>
                 </p>
                 <Button variant="secondary" href="/empiece">Empiece</Button>
             </Card.Body>
@@ -123,15 +124,35 @@ function Home() {
         <Container className="py-3 px-1">
         <Card bg="light">
             <Card.Header>
-                <h4 className="p-1">Nativo</h4>
+                <h4 className="p-1 pt-2">Traducción Profesional</h4>
+            </Card.Header>
+            <Card.Body>
+                <p className="p-2 pt-md-3">
+                    A pesar de la mejora de la traducción automática, es todavía obvio cuando ha usado el traductor de Google - no tiene fluidez y es dificil etender.
+                </p>
+                <p className="p-2">
+                    Con este servicio, su hoja de vida es traducido de español por nuestro equipo de hispanohablantes bilingües y luego editado por nuestro equipo angloparlante.
+                    El resultado es <strong>un CV en inglés fluido y nativo con exactamente el mismo sentido que su hoja de vida en español</strong>.
+                </p>
+                <p className="p-2">
+                <strong>Incluye también formato impesionante y edición de copia.</strong>
+                </p>
+                <p className="p-2">
+                    <Button variant="secondary" href="/empiece">Empiece</Button>
+                </p>
+            </Card.Body>
+        </Card>
+        </Container>
+
+        <Container className="py-3 px-1">
+        <Card bg="light">
+            <Card.Header>
+                <h4 className="p-1">Inglés Nativo</h4>
             </Card.Header>
             <Card.Body>
                 <p className="p-2 pt-md-3">
                 Nuestro equipo de editores nativos angloparlantes modifica su hoja de vida hasta que el inglés está fluido y nativo.
                 Con este servicio <strong>eliminamos toda la evidencia que no es angloparlante nativo</strong> sin cambiar que quieres decir.
-                </p>
-                <p className="p-2">
-                Si eliges que traduzcamos su CV, necesita este servicio o el servicio Profesional.
                 </p>
                 <p className="p-2">
                 <strong>Incluye también formato Impesionante.</strong>
@@ -141,46 +162,6 @@ function Home() {
         </Card>
         </Container>
 
-        <Container className="py-3 px-1">
-        <Card bg="light">
-            <Card.Header>
-                <h4 className="p-1 pt-2">Impresionante</h4>
-            </Card.Header>
-            <Card.Body>
-                <p className="p-2 pt-md-3">
-                    Formato profesional y impresionante de su hoja de vida, con opciones de estilo y color. Resulta en un PDF de alta calidad.
-                </p>
-                <p className="p-2">
-                    No areglamos problemas de gramatica y fluidez.
-                </p>
-                <p className="p-2">
-                    <Button variant="secondary" href="/empiece">Empiece</Button>
-                </p>
-            </Card.Body>
-        </Card>
-        </Container>
-
-        <Container className="py-3 px-1">
-        <Card bg="light">
-            <Card.Header>
-                <h4 className="p-1 pt-2">Otros Servicios</h4>
-            </Card.Header>
-            <Card.Body>
-                <p className="p-2 pt-md-3">
-                    Si prefiere escrbir en español, podemos traducir su hoja de vida a inglés por un cargo adicional con los servcios Nativo y Profesional. 
-                </p>
-                <p className="p-2">
-                    <Button variant="secondary" href="/empiece">Empiece</Button>
-                </p>
-                <p className="p-2">
-                    Todos nuestros servicios puden ser aplicados a una carta de presentacion también, cargado por palabra.
-                </p>
-                <p className="p-2">
-                    <Button variant="secondary" href="/empiece">Empiece</Button>
-                </p>
-            </Card.Body>
-        </Card>
-        </Container>
 
         </Container>
         </Container>
